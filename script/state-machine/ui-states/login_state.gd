8bf865e746668d289f80a1c32f6c1fa20a39b500
Florian <rea.florian@gmail.com>
2025-03-14 08:59:59 -0400

    Fix double login when login with username and password

--- script/state-machine/ui-states/login_state.gd
+++ script/state-machine/ui-states/login_state.gd
@@ -8,16 +8,26 @@ extends BaseState
 
 
 func enter() -> void:
-	Firebase.Auth.login_succeeded.connect(_on_login_succeeded)
+	Firebase.Auth.login_succeeded.connect(_on_first_login_succeeded)
 
 
 func exit() -> void:
-	Firebase.Auth.login_succeeded.disconnect(_on_login_succeeded)
+	if Firebase.Auth.login_succeeded.is_connected(_on_first_login_succeeded):
+		Firebase.Auth.login_succeeded.disconnect(_on_first_login_succeeded)
+	if Firebase.Auth.login_succeeded.is_connected(_on_real_login_succeeded):
+		Firebase.Auth.login_succeeded.disconnect(_on_real_login_succeeded)
 
 
-func _on_login_succeeded(auth: Dictionary) -> void:
+## Auth data with the first login with username / password does not contain the access token. So we use
+## Firebase.Auth.check_auth_file() to get it.
+func _on_first_login_succeeded(auth: Dictionary) -> void:
+	Firebase.Auth.login_succeeded.disconnect(_on_first_login_succeeded)
+	Firebase.Auth.login_succeeded.connect(_on_real_login_succeeded)
 	Firebase.Auth.save_auth(auth)
 	Firebase.Auth.check_auth_file()
+
+
+func _on_real_login_succeeded(auth: Dictionary) -> void:
 	FireBaseConf.userId = AppData.get_user_id()
 	FireBaseConf.Init(auth)
 	var is_first_login := await UserService.is_first_login()
